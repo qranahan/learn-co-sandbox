4cea0e6cdc95970dadd5c12a965a350d79adb5e4
Quinn Ranahan <quinn.ranahan@gmail.com>
2018-07-05 18:19:34 +0000

    Done.

--- practice.js
+++ practice.js
@@ -1,1 +1,7 @@
-;1+1=
+var myVariable = "hot dogs" // assigns "hot dogs"
+myVariable 
+// > "hot dogs"
+ 
+myVariable = "veggie burgers" // reassigns the variable to "veggie burgers"
+myVariable
+// > "veggie burgers"
